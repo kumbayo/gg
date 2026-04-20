07654266786d52a03db8c8a94037f15c32938136
Peter Oberndorfer <kumbayo84@arcor.de>
2026-04-20 23:16:00 +0200

    Fix incorrect op log message when moving a bookmark
    Previously we would see
    point RefNameBuf("master") to commit ed2fd5c8172cfa4643b395cdd8952425e9ff884e
    now we see
    point "master" to commit ed2fd5c8172cfa4643b395cdd8952425e9ff884e
    
    This problem exists since 01db853da43a00417b5432c4aa7886561ed4ca3d

--- src/worker/mutations/ref.rs
+++ src/worker/mutations/ref.rs
@@ -425,7 +425,7 @@ impl Mutation for MoveRef {
                         tx,
                         format!(
                             "point {:?} to commit {}",
-                            &bookmark_name_ref,
+                            &bookmark_name_ref.as_str(),
                             commit.id().hex()
                         ),
                     )
